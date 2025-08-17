c24831a33615cb31cfb553cb10168eacdb6c052c
Nicole D'Anton <nicoledanton7@gmail.com>
2025-08-16 19:56:56 -0500

    added click event on products to show the price

--- scripts/Products.js
+++ scripts/Products.js
@@ -1,4 +1,4 @@
-import { getProducts } from "./database.js"
+import { getOrders, getProducts } from "./database.js"
 
 const products = getProducts()
 
@@ -6,11 +6,27 @@ export const Products = () => {
     let html = "<ul>"
 
     for (const product of products) {
-        html += `<li>${product.name}</li>`
+        html += `<li data-type="product"
+                    data-name="${product.name}"
+                    data-price="${product.price}">
+                    ${product.name}
+                    </li>`
     }
 
     html += "</ul>"
 
     return html
 }
+
+
+document.addEventListener(
+    "click",
+    (clickEvent1) => {
+        const itemClicked = clickEvent1.target
+        if (itemClicked.dataset.type === "product") {
+            window.alert (`${itemClicked.dataset.name} costs \n $${itemClicked.dataset.price}`)
+            }
+    }
+
+)
 
