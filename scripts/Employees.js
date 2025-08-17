f1fcdce9854bfe6b3a87d1d9f2b9da9fe8446f38
Nicole D'Anton <nicoledanton7@gmail.com>
2025-08-16 21:18:41 -0500

    added click event for showing number of orders when clicking on employee

--- scripts/Employees.js
+++ scripts/Employees.js
@@ -1,4 +1,4 @@
-import { getEmployees } from "./database.js"
+import { getEmployees, getOrders } from "./database.js"
 
 const employees = getEmployees()
 
@@ -6,11 +6,33 @@ export const Employees = () => {
     let html = "<ul>"
 
     for (const employee of employees) {
-        html += `<li>${employee.name}</li>`
+        html += `<li data-type="employee"
+                    data-id="${employee.id}"
+                    data-name="${employee.name}">
+                    ${employee.name}
+                     </li>`
     }
 
     html += "</ul>"
 
     return html
 }
+
+
+document.addEventListener(
+    "click",
+    (clickEvent) => {
+        const itemClicked = clickEvent.target
+        if (itemClicked.dataset.type === "employee") {
+            const orders = getOrders()
+            let orderCounter = 0
+            for (const order of orders){
+                if (order.employeeId === parseInt(itemClicked.dataset.id)){
+                    orderCounter ++
+                }
+            }
+            window.alert(`${itemClicked.dataset.name} has ${orderCounter} orders`)
+        }
+    }
+)
 
